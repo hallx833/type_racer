5b9518a6d02ff88b417194004da9bf54deb3490b
Evan Hall <hallx833@umn.edu>
2020-10-11 20:17:20 -0500

    Added automatic start

--- type_racer.py
+++ type_racer.py
@@ -2,19 +2,26 @@
 from PIL import Image
 import pytesseract as pyt
 import pyautogui
-import time
+from pynput import mouse
+
+# %%
+def on_click(x, y, button, pressed):
+    if button == mouse.Button.left:
+        return False
 
 # %%
 top = pyautogui.locateOnScreen('top.png', grayscale=True)
 bottom = pyautogui.locateOnScreen('bottom.png')
-text_im = pyautogui.screenshot(region = (top.left, top.top+top.height+70, bottom.width, bottom.top-top.top-top.height-70))
+text_im = pyautogui.screenshot(region = (top.left-3, top.top+top.height+70, bottom.width, bottom.top-top.top-top.height-70))
 
 text = pyt.image_to_string(text_im)
 text = text.replace('\n', ' ')
 text = text.replace('|', 'I')
 
 # %%
-time.sleep(1)
-pyautogui.write(text, interval=0.03)
+listener = mouse.Listener(on_click=on_click)
+listener.start()
+listener.join()
+pyautogui.write(text, interval=0.01)
 
-
+# %%
